faf2bef6bfe7e4738cb64de81212c73bbb346a16
VIVIES Antoine <antoine.vivies@epitech.eu>
2017-12-19 13:15:24 +0100

    Pemière version stable

---
+++ script.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+if [ $# -eq 0 ]
+  then
+      echo "No arguments supplied"
+      exit 84
+fi
+login=$1
+sudo pacman -S git
+sudo pacman -S yaourt
+
+cd installation
+./install $1
+cd ~
